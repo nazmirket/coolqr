c9d833adce29c82160bdf91b5b4a63698ebf7ef1
Nazmi Yılmaz <nazmi.yilmaz@mirket.dev>
2025-07-18 23:47:09 +0300

    add changes

--- demo/ecosystem.config.js
+++ demo/ecosystem.config.js
@@ -1,11 +1,14 @@
 module.exports = {
 	apps: [
 		{
-			name: 'qr',
-			port: '9491',
+			name: 'coolqr',
+			port: 9491,
 			exec_mode: 'cluster',
-			instances: 'max',
+			instances: 1,
 			script: './.output/server/index.mjs',
+			env: {
+				NODE_ENV: 'production',
+			},
 		},
 	],
 }
